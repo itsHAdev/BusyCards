e0484d195389a63b2f2e33162c826863edcb4961
Hadeel <ha.ada1@hotmail.com>
2025-11-30 21:32:45 +0300

    MVVM+Assets

---
+++ BusyCards/MVVM/Views/AuditoryView.swift
@@ -0,0 +1,19 @@
+//
+//  AuditoryView.swift
+//  BusyCards
+//
+//  Created by Hadeel Alansari on 30/11/2025.
+//
+
+import SwiftUI
+
+struct AuditoryView: View {
+    var body: some View {
+        
+        
+    }
+}
+
+#Preview {
+    AuditoryView()
+}
--- BusyCards/MVVM/Views/ContentView.swift
+++ BusyCards/MVVM/Views/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
             
             Text("Hello, world!")
+                
         }
         .padding()
     }
