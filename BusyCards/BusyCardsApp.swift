16ae4e6f8f59d0e60fbfd34499b60530a8fe944a
Hadeel <ha.ada1@hotmail.com>
2025-11-30 20:46:27 +0300

    Initial Commit

---
+++ BusyCards/BusyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusyCardsApp.swift
+//  BusyCards
+//
+//  Created by Hadeel Alansari on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BusyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusyCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusyCards
+//
+//  Created by Hadeel Alansari on 30/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
