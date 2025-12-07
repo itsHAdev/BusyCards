8ba190fb4776882152dd66ae2104e5213fbb6415
Hadeel <ha.ada1@hotmail.com>
2025-12-07 10:42:12 +0300

    AuditoryView+AuditoryView2

--- BusyCards/MVVM/Views/AuditoryView.swift
+++ BusyCards/MVVM/Views/AuditoryView.swift
@@ -9,63 +9,100 @@ import SwiftUI
 
 struct AuditoryView: View {
     var body: some View {
-        
-        ZStack{
+        NavigationStack{
             
-            Color.background
-                .ignoresSafeArea()
-            
-            VStack(spacing: 75){
+            ZStack{
                 
-                Image("Auditory2")
+                Color.background
+                    .ignoresSafeArea()
                 
-                
-                Button{}label: {
+                VStack(spacing: 152){
                     
-                    ZStack{
-                    Color.darkBlue
-                        .frame(width: 240,height: 244)
-                        .cornerRadius(300)
-                        .shadow(color: Color.black , radius: 4, x:2 ,y: 2)
-                        
-                        Color.darkBlue
-                            .frame(width: 240,height: 244)
-                            .cornerRadius(300)
-                            .shadow(color: Color.black , radius: 4, x:-1.6 ,y: -1.6)
-                        
-                        Color.darkBlue
-                            .frame(width: 221,height: 225)
-                            .cornerRadius(300)
-                            .opacity(0.6)
-                            .shadow(color: Color.black , radius: 5, x:2 ,y: 2)
-                        
-                        Image(systemName:"play.fill")
-                            .font(.system(size: 100))
-                            .foregroundStyle(Color.white)
-                }//z
-                }//b
-                
-                Button{}label: {
+                    Image("Auditory2")
                     
-                    ZStack{
-                        
-                        Color.darkBlue
-                            .frame(width: 260,height: 56)
-                            .cornerRadius(15)
-                            .shadow(color: Color.black, radius: 3,x: 2,y: 2)
-                        
-                        Text("انتهيت")
-                            .foregroundStyle(Color.white)
-                            .font(.system(size: 28))
-                        
-                    }//z
-                }//b
-            }//v
-            
-        }//z
+                    
+                    Text("جدول ضرب 2")
+                        .font(.system(size: 34))
+                    
+                    NavigationLink {
+                        AuditoryView2()
+                    } label: {
+                        ZStack{
+                            Color.darkBlue
+                                .frame(width: 260,height: 56)
+                                .cornerRadius(15)
+                                .shadow(color: Color.black, radius: 3,x: 2,y: 2)
+                            
+                            Text("استمع للإجابة")
+                                .foregroundStyle(Color.white)
+                                .font(.system(size: 28))
+                        }
+                    }
+                }//v
+                
+            }//z
+        }
     }
 }
 
 #Preview {
     AuditoryView()
+}
+
+import SwiftUI
+
+struct AuditoryView2: View {
+  
+    var body: some View {
+       
+            
+            ZStack{
+                
+                Color.background
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 68){
+                    
+                    Image("Auditory2")
+                    
+                    Button{
+                        
+                    }label: {
+                        
+                        ZStack{
+                            
+                            Color.darkBlue
+                                .frame(width: 218,height: 218)
+                                .cornerRadius(1000)
+                            
+                            Color.darkBlue2
+                                .frame(width: 145.21,height: 145.21)
+                                .cornerRadius(1000)
+                            
+                            Image(systemName:"play.fill")
+                                .font(.system(size: 60))
+                                .foregroundStyle(Color.white)
+                        }//z
+                    }
+                    
+                    NavigationLink {
+                        HomePage()
+                        
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        ZStack {
+                            Color.darkBlue
+                                .frame(width: 260, height: 56)
+                                .cornerRadius(15)
+                                .shadow(color: Color.black, radius: 3, x: 2, y: 2)
+                            
+                            Text("انتهيت")
+                                .foregroundStyle(Color.white)
+                                .font(.system(size: 28))
+                        }
+                    }//b
+                    
+                }//v
+            }//z
+    }
 }
